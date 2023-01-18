d4379de4f156687ca4ac894074dd104bf0c24aea
Fletcher Hardison <fhardison@@gmail.com>
2023-01-18 11:26:42 -0500

    fixing problem

--- python3/cyclebreathing/cycle.py
+++ python3/cyclebreathing/cycle.py
@@ -4,6 +4,59 @@ from greek_normalisation.utils import nfc, nfd
 from greek_accentuation.characters import add_breathing, Breathing,Accent, add_diacritic 
 import re
 
+ACCENTS = [chr(int(x, 16)) for x in ['0301', '0300', '0342']]
+BREATHINGS = [chr(int(x, 16)) for x in ['0313', '0314']]
+SMOOTH = BREATHINGS[0]
+DAERESIS = chr(int('0308', 16))
+
+# IOTA SUBSCRIPT is last but is 0345
+
+VOWELS = [x for x in "αεηιυοω"]
+DIPTHONGS = [x for x in "αι ει ου ευ αυ ηυ οι".split(' ')] 
+
+# canonical order is letter < breathing < accent < iota subscript
+
+def fix_text(text):
+    out = []
+    for w in nfd(text).split(' '):
+        #VV
+        if w[0:2] in DIPTHONGS:
+            #already has breathing mark
+            if w[2] in BREATHINGS:
+                out.append(w)
+            #needs smooth
+            elif w[2] in ACCENTS:
+                w = w[0:2] + SMOOTH +  w[2:]
+                out.append(w)
+            else:
+                out.append(w[0:2] + SMOOTH + w[2:])
+        #VA
+        elif w[0] in VOWELS and w[1] in ACCENTS:
+
+            if len(w) < 3:
+                out.append((w[0:1] + SMOOTH + w[1:]))
+            elif w[2] not in BREATHINGS:
+                out.append((w[0:1] + SMOOTH + w[1:]))
+            else:
+                out.append(w)
+        #VVAB
+        #VVD
+        elif len(w) > 2 and w[0] in VOWELS and w[2] == DAERESIS:
+            out.append((w[0] + SMOOTH + w[1:]))
+        elif w[0] in VOWELS:
+            if len(w) == 1:
+                out.append(w[0] + SMOOTH)
+            elif w[1] not in BREATHINGS:
+                out.append((w[0] + SMOOTH + w[1:]))
+            elif w[1] in ACCENTS:
+                out.append((w[0] + SMOOTH + w[1:]))
+            else:
+                out.append(w)
+                print(w)
+        else: 
+            out.append(w)
+    return ' '.join(out)
+
 
 class Cycle():
     FIND = [x for x in "α ε η υ ο ω i".split(' ') if x]
@@ -36,63 +89,11 @@ class Cycle():
             for x, y in l:
                 text = re.sub('\\b' + x, y, text)
         return text.replace('DIPTHDIPTH', '')
-       
-    ACCENTS = [chr(int(x, 16)) for x in ['0301', '0300', '0342']]
-    BREATHINGS = [chr(int(x, 16)) for x in ['0313', '0314']]
-    SMOOTH = BREATHINGS[0]
-    DAERESIS = chr(int('0308', 16))
+    
 
-    # IOTA SUBSCRIPT is last but is 0345
-
-    VOWELS = [x for x in "αεηιυοω"]
-    DIPTHONGS = [x for x in "αι ει ου ευ αυ ηυ οι".split(' ')] 
-
-    # canonical order is letter < breathing < accent < iota subscript
-
-    def fix_text(self, text):
-        out = []
-        for w in nfd(text).split(' '):
-            #VV
-            if w[0:2] in DIPTHONGS:
-                #already has breathing mark
-                if w[2] in BREATHINGS:
-                    out.append(w)
-                #needs smooth
-                elif w[2] in ACCENTS:
-                    w = w[0:2] + SMOOTH +  w[2:]
-                    out.append(w)
-                else:
-                    out.append(w[0:2] + SMOOTH + w[2:])
-            #VA
-            elif w[0] in VOWELS and w[1] in ACCENTS:
-
-                if len(w) < 3:
-                    out.append((w[0:1] + SMOOTH + w[1:]))
-                elif w[2] not in BREATHINGS:
-                    out.append((w[0:1] + SMOOTH + w[1:]))
-                else:
-                    out.append(w)
-            #VVAB
-            #VVD
-            elif len(w) > 2 and w[0] in VOWELS and w[2] == DAERESIS:
-                out.append((w[0] + SMOOTH + w[1:]))
-            elif w[0] in VOWELS:
-                if len(w) == 1:
-                    out.append(w[0] + SMOOTH)
-                elif w[1] not in BREATHINGS:
-                    out.append((w[0] + SMOOTH + w[1:]))
-                elif w[1] in ACCENTS:
-                    out.append((w[0] + SMOOTH + w[1:]))
-                else:
-                    out.append(w)
-                    print(w)
-            else: 
-                out.append(w)
-        return ' '.join(out)
- 
     def test(self):
         val = '\n'.join(list(vim.current.buffer))
         # new = self.run_replacements(val)
-        new = self.fix_text(val)
+        new = fix_text(val)
         vim.current.buffer[:] = new.split('\n')
 
